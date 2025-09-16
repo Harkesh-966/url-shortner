5a3bea2c614f226a98fa772f5e358cea5963baa8
Harkesh-966 <harkeshsaini108@gmail.com>
2025-09-16 10:48:50 +0530

    delete test

--- tests/integration/app.test.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-import app from '../../src/app';
-
-describe('Health basics', () => {
-  it('returns 404 for missing code', async () => {
-    const res = await request(app).get('/nope-code-xyz');
-    expect([302,404,410]).toContain(res.statusCode);
-  });
-});
--- tests/unit/shortcode.test.ts
+++
@@ -1,6 +0,0 @@
-import { generateShortCode } from '../../src/utils/shortcode';
-
-test('generates codes of given length', () => {
-  const code = generateShortCode(8);
-  expect(code).toHaveLength(8);
-});
